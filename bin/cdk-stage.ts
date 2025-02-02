9c845bb5def4677f53f1ef9393fcbec00934e59d
seitamuro <seitamuro0210@gmail.com>
2025-02-02 23:42:30 +0900

    devステージのテーブルをdestroy時に強制削除するようにした

--- bin/cdk-stage.ts
+++ bin/cdk-stage.ts
@@ -1,20 +1,43 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { CdkStageStack } from '../lib/cdk-stage-stack';
+import * as cdk from "aws-cdk-lib";
+import { CfnRepository } from "aws-cdk-lib/aws-ecr";
+import { IConstruct } from "constructs";
+import { CdkStageStack } from "../lib/cdk-stage-stack";
 
 const app = new cdk.App();
-new CdkStageStack(app, 'CdkStageStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const availableStages = ["dev", "stg", "prod"] as const;
+type Stage = (typeof availableStages)[number];
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+const isValidStage = (stage: string): stage is Stage => {
+  return availableStages.includes(stage as Stage);
+};
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+const stage = app.node.tryGetContext("stage");
+if (!isValidStage(stage)) {
+  throw new Error(`Invalid stage: ${stage}`);
+}
+
+cdk.Tags.of(app).add("Project", "MyProject");
+
+new CdkStageStack(app, `CdkStageStack-${stage}`, {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
 });
+
+class EnforceDeletionPolicy implements cdk.IAspect {
+  public visit(node: IConstruct): void {
+    if (node instanceof cdk.CfnResource) {
+      node.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY);
+    }
+    if (node instanceof CfnRepository) {
+      node.emptyOnDelete = true;
+    }
+  }
+}
+
+if (stage === "dev") {
+  cdk.Aspects.of(app).add(new EnforceDeletionPolicy());
+}
--- lib/cdk-stage-stack.ts
+++ lib/cdk-stage-stack.ts
@@ -1,16 +1,14 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkStageStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkStageQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const table = new dynamodb.Table(this, "Table", {
+      partitionKey: { name: "id", type: dynamodb.AttributeType.STRING },
+    });
   }
 }
